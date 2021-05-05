d82eaa8522d245c49222ca5346e6dd04df342db5
kunal8164705 <kunal8164705@gmail.com>
2021-05-05 22:28:17 +0530

    dark mode fuctionality added

---
+++ src/common/containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DarkModeProvider from '../contexts/DarkModeProvider';
+
+export default function App({ children }) {
+  return (
+    <DarkModeProvider>
+      {children}
+    </DarkModeProvider>
+  );
+}
